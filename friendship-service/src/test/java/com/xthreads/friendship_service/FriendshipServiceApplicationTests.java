ed6ff6931116f8b0010a2cd12cfbf644a5a6ce18
thieutrung2k3 <whyiam200@gmail.com>
2024-11-26 22:30:40 +0700

    first commit

---
+++ friendship-service/src/test/java/com/xthreads/friendship_service/FriendshipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xthreads.friendship_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendshipServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
